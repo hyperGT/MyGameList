ffe64c65c3c3f7158e058b2bccd67cbc7e5febf5
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-07-12 18:47:18 -0300

    Initial Commit

---
+++ src/main/java/com/fireworks/MyGameList/MyGameListApplication.java
@@ -0,0 +1,13 @@
+package com.fireworks.MyGameList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fireworks/MyGameList/MyGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fireworks.MyGameList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
