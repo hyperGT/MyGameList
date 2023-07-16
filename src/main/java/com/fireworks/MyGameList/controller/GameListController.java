79b0816e843076c8c17635c3a27ce958dfdacb29
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-07-16 17:12:10 -0300

    game move on list

--- src/main/java/com/fireworks/MyGameList/controller/GameListController.java
+++ src/main/java/com/fireworks/MyGameList/controller/GameListController.java
@@ -2,13 +2,11 @@ package com.fireworks.MyGameList.controller;
 
 import com.fireworks.MyGameList.data.GameListDTO;
 import com.fireworks.MyGameList.data.GameMinDTO;
+import com.fireworks.MyGameList.data.ReplacementDTO;
 import com.fireworks.MyGameList.service.GameListService;
 import com.fireworks.MyGameList.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,7 @@ public class GameListController {
     @Autowired
     private GameService gameService;
 
+    // retorna todas as listas e os seus objetos
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> response = gameListService.findAll();
@@ -31,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         List<GameMinDTO> response = gameService.findByList(listId);
         return response;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId,@RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
--- src/main/java/com/fireworks/MyGameList/service/GameListService.java
+++ src/main/java/com/fireworks/MyGameList/service/GameListService.java
@@ -2,7 +2,9 @@ package com.fireworks.MyGameList.service;
 
 import com.fireworks.MyGameList.data.GameListDTO;
 import com.fireworks.MyGameList.entities.GameList;
+import com.fireworks.MyGameList.projection.GameMinProjection;
 import com.fireworks.MyGameList.repository.GameListRepository;
+import com.fireworks.MyGameList.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> list = gameListRepository.findAll();
         List<GameListDTO> dtoList = list.stream().map(x -> new GameListDTO(x)).toList();
         return dtoList;
     }
+
+    @Transactional()
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        // se o valor da posição de origem for menor que o da posição de destino, o valor minimo/menor é o da origem
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for(int i = min ; i <= max ; i++){  //max = 8, min = 5
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
+
 }
