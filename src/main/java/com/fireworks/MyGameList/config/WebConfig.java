7af086b7ded1b2fab46332b72c98f4f5b963e052
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-07-16 14:52:02 -0300

    cors config

---
+++ src/main/java/com/fireworks/MyGameList/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.fireworks.MyGameList.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+}
